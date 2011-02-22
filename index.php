1b53d48913b431d90ae5f062be9b5637b5847aa3
Lode Claassen <lode@alsvanzelf.nl>
2011-02-22 13:43:38 +0100

    don't show disabled or hidden worlds in the results updated the looks & styles

--- index.php
+++ index.php
@@ -28,6 +28,20 @@ function sqlGetResults($query, $handle) {
 	return $results;
 }
 
+function skipHiddenWorlds($world_results, $worlds) {
+	$new_results = array();
+	foreach ($world_results as $key => $world) {
+		$uni['mysql']['handle'] = new mysqli($worlds['mysql']['host'], $worlds['mysql']['user'], $worlds['mysql']['pass'], 'aw_universe');
+		$uni['mysql']['query'] = "SELECT Name, Hidden FROM awu_license WHERE Name LIKE '".$world['Name']."';";
+		$world_info = sqlGetResults($uni['mysql']['query'], $uni['mysql']['handle']);
+		if ($world_info[0]['Hidden'] != 1) {
+			$new_results[$key] = $world;
+		}
+	}
+	
+	return $new_results;
+}
+
 $query = '';
 if (!empty($_GET['q'])) {
 	$query = $_GET['q'];
@@ -50,11 +64,16 @@ if (!empty($_GET['q'])) {
 		$worlds['mysql']['name'] = 'aw_w'.$worldserver;
 		$worlds['mysql']['handle'] = new mysqli($worlds['mysql']['host'], $worlds['mysql']['user'], $worlds['mysql']['pass'], $worlds['mysql']['name']);
 		$worlds['mysql']['arg']['q'] = $worlds['mysql']['handle']->real_escape_string($query);
-		$worlds['mysql']['query'] = "SELECT ID, Name FROM aws_world WHERE Name LIKE '%".$worlds['mysql']['arg']['q']."%';";
+		$worlds['mysql']['query'] = "SELECT ID, Name FROM aws_world WHERE Enabled = '1' AND Name LIKE '%".$worlds['mysql']['arg']['q']."%';";
 		$worlds['resultsName'] = array_merge($worlds['resultsName'], sqlGetResults($worlds['mysql']['query'], $worlds['mysql']['handle']));
-		$worlds['mysql']['query'] = "SELECT Name, Value FROM aws_world, aws_attrib WHERE aws_world.ID = aws_attrib.World AND aws_attrib.ID = '58' AND Value LIKE '%".$worlds['mysql']['arg']['q']."%';";
+		$worlds['mysql']['query'] = "SELECT Name, Value FROM aws_world, aws_attrib WHERE aws_world.Enabled = '1' AND aws_world.ID = aws_attrib.World AND aws_attrib.ID = '58' AND Value LIKE '%".$worlds['mysql']['arg']['q']."%';";
 		$worlds['resultsKeywords'] = array_merge($worlds['resultsKeywords'], sqlGetResults($worlds['mysql']['query'], $worlds['mysql']['handle']));
 	}
+	
+	// don't show hidden worlds
+	$worlds['resultsName'] = skipHiddenWorlds($worlds['resultsName'], $worlds);
+	$worlds['resultsKeywords'] = skipHiddenWorlds($worlds['resultsKeywords'], $worlds);
+	
 	$worlds['prefixLink'] = 'http://tools.l3d.nl/teleport/';
 	// urls of scheme 'teleport:' don't work in the tabs, only in the web sidebar
 	
@@ -73,14 +92,50 @@ if (!empty($_GET['q'])) {
 	<title>L3Daw zoeken</title>
 	<style>
 	body {
+		margin: 0;
+		padding-left: 5px;
 		font-family: Verdana;
 		font-size: small;
 		font-weight: normal;
 	}
-	a {
+	
+	#header {
+		margin-left: -5px;
+		color: white;
+		background-color: #AA122B;
+	}
+	#header h1, #header a, #header a:hover, #header a:active, #header p {
+		color: white;
+	}
+	#header h1 {
+		margin-top: 0;
+		margin-bottom: 0;
+	}
+	#header a {
+		display: block;
+		padding: 0.25em;
+		padding-left: 5px;
 		text-decoration: none;
+	}
+	#header p {
+		margin-top: -0.5em;
+		padding-left: 5px;
+		padding-bottom: 0.5em;
+		text-indent: 55px;
+		font-style: italic;
+	}
+	
+	h2, h3, h4, h5, h6 {
+		color: #AA122B;
+	}
+	a {
 		color: #00E;
 	}
+	a:hover, a:active {
+		text-decoration: underline;
+		color: #AA122B;
+	}
+	
 	ul {
 		margin-left: 0;
 		padding-left: 0;
@@ -94,19 +149,48 @@ if (!empty($_GET['q'])) {
 	.highlight {
 		font-weight: bold;
 	}
+	
+	form {
+		margin-top: 0.5em;
+		margin-bottom: 0.25em;
+	}
+	#intro {
+		margin-top: 0.25em;
+		color: #888;
+		font-style: italic;
+	}
+	#intro a {
+		text-decoration: none;
+		color: #55E;
+	}
+	#intro:hover {
+		color: black;
+	}
+	#intro:hover a {
+		text-decoration: underline;
+		color: #00E;
+	}
+	#intro a:hover, #intro a:active {
+		color: #AA122B;
+	}
+	#query {
+		width: 130px;
+	}
 	</style>
 </head>
 <body>
-	<h1><!--L3Daw -->zoeken</h1>
+	<div id="header">
+		<h1><a href="./">zoeken</a></h1>
+		<p>in projecten en 3Dwiki</p>
+	</div>
 	<form method="GET">
-		<input type="text" name="q" value="<?php echo $query; ?>">
-		<input type="submit" value="Zoeken">
+		<input id="query" type="text" name="q" value="<?php echo $query; ?>">
+		<input id="submit" type="submit" value="Zoeken">
 	</form>
-	<p>bijvoorbeeld:
+	<p id="intro">voorbeeld:
 		<a href="?q=vision">vision</a>,
 		<a href="?q=kunst">kunst</a>,
-		<a href="?q=leren">leren</a>,
-		<a href="?q=robot">robot</a>
+		<a href="?q=leren">leren</a>
 	</p>
 	</ul>
 <?php
